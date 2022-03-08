5f8053cc11730b9803acd6d2146923cfee5d320f
Blake <2011264@schoolsnet.act.edu.au>
2022-03-08 15:15:12 +1100

    Code added

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
 var speed = 500
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	pass
-	
-	
+	var collidedObject = move_and_collide(Vector2(-speed*delta, 0))
+	if (collidedObject):
+		print(collidedObject.collider.name)
+		if "Enemy" in collidedObject.collider.name:
+			collidedObject.get_collider().queue_free()
+			GlobalVariables.scoringInformation["currentScore"] +=10
+			print (GlobalVariables.scoringInformation["currentScore"])
+		queue_free()		
--- Player/Player.gd
+++ Player/Player.gd
@@ -14,10 +14,16 @@ func _ready():
 	set_physics_process(true)
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("ui_up"):
+		move_and_collide(Vector2(0, -movement_speed * delta))
+	if Input.is_action_pressed("ui_down"):
+		move_and_collide(Vector2(0, movement_speed * delta))
 			
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-20)
+		get_tree().get_root().add_child(bulletInstance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
